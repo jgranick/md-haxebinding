78c4eea094cefd0afb192adb606050e284745813
Heorhiy <zaynyatyi@gmail.com>
2013-10-22 15:02:39 +0300

    Added syntax highlighting based on D plugin

---
+++ HaxeBinding/Highlighting/HaxeSyntaxMode.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Mono.TextEditor;
+using Mono.TextEditor.Highlighting;
+using System.IO;
+
+namespace MonoDevelop.HaxeBinding.Highlighting
+{
+	public class HaxeSyntaxMode : SyntaxMode
+	{
+		static SyntaxMode baseMode;
+
+		public HaxeSyntaxMode ()
+		{
+			var matches = new List<Mono.TextEditor.Highlighting.Match>();
+
+			if (baseMode == null)
+			{
+				var provider = new ResourceStreamProvider(
+					typeof(HaxeSyntaxMode).Assembly,
+					typeof(HaxeSyntaxMode).Assembly.GetManifestResourceNames().First(s => s.Contains("HaxeSyntaxHighlightingMode")));
+				using (Stream s = provider.Open())
+					baseMode = SyntaxMode.Read(s);
+			}
+
+			this.rules = new List<Rule>(baseMode.Rules);
+			this.keywords = new List<Keywords>(baseMode.Keywords);
+			this.spans = new List<Span>(baseMode.Spans.Where(span => span.Begin.Pattern != "#")).ToArray();
+			matches.AddRange(baseMode.Matches);
+			this.prevMarker = baseMode.PrevMarker;
+			this.SemanticRules = new List<SemanticRule>(baseMode.SemanticRules);
+			this.keywordTable = baseMode.keywordTable;
+			this.keywordTableIgnoreCase = baseMode.keywordTableIgnoreCase;
+			this.properties = baseMode.Properties;
+
+			// D Number literals
+			matches.Add(workaroundMatchCtor(
+				"Number"                        
+				, @"(?<!\w)(0((x|X)[0-9a-fA-F_]+|(b|B)[0-1_]+)|([0-9]+[_0-9]*)[L|U|u|f|i]*)"));
+
+			// extern linkages attributes
+			//matches.Add(workaroundMatchCtor("constant.digit", "(?<=extern[\\s]*\\()[\\s]*(C(\\+\\+)?|D|Windows|System|Pascal|Java)[\\s]*(?=\\))"));
+
+			// version checks
+			//matches.Add(workaroundMatchCtor("constant.digit", @"(?<=version[\s]*\()[\s]*(DigitalMars|GNU|LDC|Windows|OSX|linux|FreeBSD|OpenBSD|BSD|Solaris|Posix|D_Version2)[\s]*(?=\))"));
+
+			// type declaration names
+			//matches.Add(workaroundMatchCtor("keyword.semantic.type", @"(?<=(class|struct|union|interface|template)[\s]+)[\w]+"));
+
+
+
+			this.matches = matches.ToArray();
+		}
+
+		static Match workaroundMatchCtor(string color, string regex)
+		{
+			var st = new StringReader("<Match color = \""+color+"\"><![CDATA["+regex+"]]></Match>");
+
+			var x = new XmlTextReader(st);
+			x.Read();
+			var m=Match.Read(x);
+			st.Close();
+
+			return m;
+		}
+	}
+}
